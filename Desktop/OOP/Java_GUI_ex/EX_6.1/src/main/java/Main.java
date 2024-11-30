65d83c06d96323808ab654b184f79502342f9691
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-11-30 19:35:28 +0200

    exercise 6.1

---
+++ Desktop/OOP/Java_GUI_ex/EX_6.1/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import View.DictionaryView;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryView.main(args);
+    }
+}
---
+++ Desktop/OOP/Java_GUI_ex/EX_6.1/src/main/java/Model/Dictionary.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.HashMap;
+
+public class Dictionary {
+    private HashMap<String, String> dictionary;
+
+    public Dictionary() {
+        this.dictionary = new HashMap<>();
+    }
+
+    public void addWord(String word, String meaning) {
+        dictionary.put(word.toLowerCase(), meaning);
+    }
+
+    public String searchWord(String word) {
+        return dictionary.getOrDefault(word.toLowerCase(), "Word not found in the dictionary.");
+    }
+}
